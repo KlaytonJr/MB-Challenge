0b92e89187fcfb39926ba095c59c46325d3661c4
Klayton Junior <klaytonjunior@protonmail.com>
2024-08-28 18:47:54 -0300

    chore: init frontend development

---
+++ frontend/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import StepForm from './components/StepForm.vue'
+</script>
+
+<template>
+  <StepForm />
+</template>
+
+<style scoped>
+</style>
---
+++ frontend/src/components/StepForm.vue
@@ -0,0 +1,12 @@
+<script script>
+</script>
+
+<template>
+    <div>
+        <h1>StepForm</h1>
+    </div>
+</template>
+    
+<style scoped>
+</style>
+  
