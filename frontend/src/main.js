487acc13c962408888386e0a6973bfcc95a482c8
Klayton Junior <klaytonjunior@protonmail.com>
2024-08-29 14:20:37 -0300

    chore: install vue-snacks a lib of toast notification

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import VSnacks from "vue-snacks";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(VSnacks);
+
+app.mount('#app');
